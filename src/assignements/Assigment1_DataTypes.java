279171aa5f6b9ff4513b7d50b39e03a556987d80
Kewal Jadhav <jadhav.kewal12@gmail.com>
2025-12-16 14:48:26 +0530

    Assignment 1- Data Types

---
+++ src/assignements/Assigment1_DataTypes.java
@@ -0,0 +1,13 @@
+package assignements;
+
+public class Assigment1_DataTypes {
+
+	public static void main(String[] args) {
+		//1. Temperature of a city in degrees Celsius: 25.5
+		
+		float temprature =25.5f;
+	System.out.println("Temperature of a city in degrees Celsius:"+temprature);
+
+	}
+
+}
